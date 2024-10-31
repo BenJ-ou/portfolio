741935475e9560cdcf5cfe8f8fc81b723ded240a
BenjaminPrince <prince-benjamin@hotmail.com>
2024-10-31 15:27:55 +0100

    feat(porfolio): maj navbar css + jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,14 +5,13 @@ const Navbar = () => {
   return (
     <nav>
       <div className="profile">
-        <img src="./src/image/image.png" alt="profile" />
+        <img src="/image/image.png" alt="profile" className="profile-img" />
         <h1 className="font-semibold">PRINCE BENJAMIN</h1>
       </div>
       <ul>
         <li><a href="#projects-section">Mes projets</a></li>
-        <li><a target="_blank" href="src/image/Dev-web-Intégrateur-CV.pdf">Curriculum Vitae</a></li>
+        <li><a target="_blank" href="/image/Dev-web-Intégrateur-CV.pdf">Curriculum Vitae</a></li>
         <li><a target="_blank" href="https://github.com/BenjaminPrince">Github</a></li>
-        <li><button id="themeLogo">&#9789;</button></li>
       </ul>
     </nav>
   );
