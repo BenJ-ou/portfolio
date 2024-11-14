b1890db06942311b1af35045af43502f66469a1d
BenjaminPrince <prince-benjamin@hotmail.com>
2024-11-14 13:28:24 +0100

    feat(porfolio):  add contact correction

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -9,12 +9,13 @@ const Contact = () => {
     message: '',
   });
   const [isSent, setIsSent] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(''); // Ajout de l'état pour gérer les erreurs
 
   useEffect(() => {
     const script = document.createElement('script');
     script.src = "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js";
     script.onload = () => {
-      emailjs.init('-YFTIKWXB8p5uB2Q_'); 
+      emailjs.init('-YFTIKWXB8p5uB2Q_'); // Votre clé publique
     };
     document.body.appendChild(script);
   }, []);
@@ -29,21 +30,22 @@ const Contact = () => {
 
   const sendEmail = (e) => {
     e.preventDefault();
-    window.emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', formState)
+    setErrorMessage(''); // Réinitialisation du message d'erreur
+    window.emailjs.send('service_ny4vy5k', 'template_48lmbap', formState)
       .then(
         (result) => {
           console.log(result.text);
-          setIsSent(true);
-          setFormState({ name: '', email: '', message: '' });
+          setIsSent(true); // Message envoyé
+          setFormState({ name: '', email: '', message: '' }); // Réinitialisation du formulaire
         },
         (error) => {
           console.log(error.text);
+          setErrorMessage('Une erreur est survenue lors de l\'envoi du message.'); // Affichage du message d'erreur
         }
       );
   };
 
   return (
-    
     <div className="contact-container">
       <h2 className="contact-title">Prendre contact</h2>
       {isSent ? (
@@ -85,6 +87,8 @@ const Contact = () => {
           <button className="form-button" type="submit">Envoyer</button>
         </form>
       )}
+      {/* Affichage du message d'erreur si il y en a */}
+      {errorMessage && <p className="error-message">{errorMessage}</p>}
     </div>
   );
 };
