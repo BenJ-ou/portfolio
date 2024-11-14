1bab24e2cf69e10d07bf839d16e12a2dc1366d66
BenjaminPrince <prince-benjamin@hotmail.com>
2024-11-14 13:55:47 +0100

    feat(porfolio):  add contact correction

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,93 +3,66 @@ import '../styles/Contact.css';
 import emailjs from 'emailjs-com';
 
 const Contact = () => {
-  const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
   const [isSent, setIsSent] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(''); // Ajout de l'état pour gérer les erreurs
+  const [errorMessage, setErrorMessage] = useState('');
+  const [buttonText, setButtonText] = useState('Envoyer');
 
   useEffect(() => {
     const script = document.createElement('script');
     script.src = "https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js";
     script.onload = () => {
-      emailjs.init('-YFTIKWXB8p5uB2Q_'); // Votre clé publique
+      emailjs.init('-YFTIKWXB8p5uB2Q_');
     };
     document.body.appendChild(script);
   }, []);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormState((prevState) => ({
-      ...prevState,
-      [name]: value,
-    }));
-  };
-
-  const sendEmail = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    setErrorMessage(''); // Réinitialisation du message d'erreur
-    window.emailjs.send('service_ny4vy5k', 'template_48lmbap', formState)
-      .then(
-        (result) => {
-          console.log(result.text);
-          setIsSent(true); // Message envoyé
-          setFormState({ name: '', email: '', message: '' }); // Réinitialisation du formulaire
-        },
-        (error) => {
-          console.log(error.text);
-          setErrorMessage('Une erreur est survenue lors de l\'envoi du message.'); // Affichage du message d'erreur
-        }
-      );
+    setButtonText('Envoi...');
+
+    const serviceID = 'default_service';
+    const templateID = 'template_48lmbap';
+
+    emailjs.sendForm(serviceID, templateID, e.target)
+      .then(() => {
+        setButtonText('Envoyer');
+        setIsSent(true);
+        setErrorMessage('');
+        e.target.reset();
+      }, (err) => {
+        setButtonText('Envoyer');
+        setErrorMessage('Une erreur est survenue lors de l\'envoi.');
+        console.error('Erreur d\'envoi:', err.text);
+      });
   };
 
   return (
-    <div className="contact-container">
-      <h2 className="contact-title">Prendre contact</h2>
-      {isSent ? (
-        <p className="success-message">Votre message a été envoyé avec succès !</p>
-      ) : (
-        <form className="contact-form" onSubmit={sendEmail}>
-          <label className="form-label">
-            Nom:
-            <input
-              className="form-input"
-              type="text"
-              name="name"
-              value={formState.name}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <label className="form-label">
-            Email:
-            <input
-              className="form-input"
-              type="email"
-              name="email"
-              value={formState.email}
-              onChange={handleChange}
-              required
-            />
-          </label>
-          <label className="form-label">
-            Message:
-            <textarea
-              className="form-textarea"
-              name="message"
-              value={formState.message}
-              onChange={handleChange}
-              required
-            ></textarea>
-          </label>
-          <button className="form-button" type="submit">Envoyer</button>
-        </form>
-      )}
-      {/* Affichage du message d'erreur si il y en a */}
-      {errorMessage && <p className="error-message">{errorMessage}</p>}
-    </div>
+    <>
+      <h1 className="contact-title">Prendre contact</h1>
+      <div className="contact-container">
+        {isSent ? (
+          <p className="success-message">Votre message a été envoyé avec succès !</p>
+        ) : (
+          <form id="form" className="contact-form" onSubmit={handleSubmit}>
+            <div className="field">
+              <label htmlFor="from_name">Nom :</label>
+              <input type="text" name="from_name" id="from_name" required />
+            </div>
+            <div className="field">
+              <label htmlFor="reply_to">Votre adresse email :</label>
+              <input type="email" name="reply_to" id="reply_to" required />
+            </div>
+            <div className="field">
+              <label htmlFor="message">Message :</label>
+              <textarea name="message" id="message" required></textarea>
+            </div>
+
+            <input type="submit" id="button" value={buttonText} />
+          </form>
+        )}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </div>
+    </>
   );
 };
 
